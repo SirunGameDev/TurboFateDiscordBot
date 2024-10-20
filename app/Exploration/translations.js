e196e6003fd7cab8e598a4794061e2fe5bf8a5c8
SirunGameDev <sirun.game.dev@gmail.com>
2024-10-20 16:42:23 +0200

    Adding further semicolons

--- app/Exploration/translations.js
+++ app/Exploration/translations.js
@@ -2,79 +2,79 @@
 export const overcome = {
     english : "overcome",
     german  : "überwinden"
-}
+};
 
 export const attack = {
     english : "attack",
     german  : "attackieren"
-}
+};
 
 export const createAdvantage = {
     english : "create Advantage",
     german  : "Vorteil erschaffen",
-}
+};
 
 export const defend = {
     english : "defend",
     german  : "verteidigen"
-}
+};
 
 export const approaches = {
     english : "approaches",
     german  : "Methoden"
-}
+};
 
 export const careful = {
     english : "careful",
     german  : "sorgfältig"
-}
+};
 
 export const clever = {
     english : "clever",
     german  : "scharfsinnig",
-}
+};
 
 export const flashy = {
     english : "flashy",
     german  : "tollkühn",
-}
+};
 
 export const forceful = {
     english : "forcecul",
     german  : "tatkräftig"
-}
+};
 
 export const quick = {
     english : "quick",
     german  : "flink"
-}
+};
 
 export const sneaky = {
     english : "sneaky",
     german  : "hinterhältig"
-}
+};
 
 export const outcome = {
     english : "outcome",
     german  : "ergebnis"
-}
+};
 
 export const fail = {
     english : "fail",
     german  : "Fehlschlag"
-}
+};
 
 export const tie = {
     english : "tie",
     german  : "Gleichstand"
-}
+};
 
 export const succeed = {
     english : "succeed",
     german  : "Erfolg"
-}
+};
 
 export const succeedwithstyle = {
     english : "succeed with style",
     german  : "voller Erfolg"
-}
+};
